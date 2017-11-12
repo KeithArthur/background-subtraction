83b6fdc90bfb0a9f85d93dde057c09ce6f4daef6
Dany Haddad <danyhaddad43@gmail.com>
2017-11-12 17:42:34 -0600

    use explicit types for graph

--- src/graph.py
+++ src/graph.py
@@ -8,10 +8,10 @@ def build_graph(frame_dimensions, batch_dimensions):
     num_x = frame_height_m - batch_height + 1
     num_y = frame_width_n - batch_width + 1
     num_groups = num_x * num_y
-    graph = {'eta_g': np.ones(num_groups),
+    graph = {'eta_g': np.ones(num_groups, dtype=np.float32),
              'groups': sp.csc_matrix(np.zeros((num_groups, num_groups)),
                                      dtype=np.bool),
-             'groups_var': sp.csc_matrix(np.zeros((frame_height_m * frame_width_n, num_groups)), dtype=np.bool)}
+             'groups_var': sp.csc_matrix(np.zeros((frame_height_m * frame_width_n, num_groups), dtype=np.bool), dtype=np.bool)}
     for i in range(0, num_groups):
         indiMatrix = np.zeros((frame_height_m, frame_width_n))
         indX = i % num_x
