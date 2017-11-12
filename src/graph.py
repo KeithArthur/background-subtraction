f337b4cf3d8d85c3440872e0c1b377433b65360d
Dany Haddad <danyhaddad43@gmail.com>
2017-11-12 14:33:44 -0600

    fix build graph and add more tests

--- src/graph.py
+++ src/graph.py
@@ -7,15 +7,15 @@ def build_graph(frame_dimensions, batch_dimensions):
     batch_width = min(batch_dimensions[1], frame_width_n)
     num_x = frame_height_m - batch_height + 1
     num_y = frame_width_n - batch_width + 1
-    num_in_group = num_x * num_y
-    graph = {'eta_g': np.ones(num_in_group),
-             'groups': sp.csc_matrix(np.zeros((num_in_group, num_in_group)),
+    num_groups = num_x * num_y
+    graph = {'eta_g': np.ones(num_groups),
+             'groups': sp.csc_matrix(np.zeros((num_groups, num_groups)),
                                      dtype=np.bool),
-             'groups_var': sp.csc_matrix(np.zeros((frame_height_m * frame_width_n, num_in_group)), dtype=np.bool)}
-    for i in range(0, num_in_group):
+             'groups_var': sp.csc_matrix(np.zeros((frame_height_m * frame_width_n, num_groups)), dtype=np.bool)}
+    for i in range(0, num_groups):
         indiMatrix = np.zeros((frame_height_m, frame_width_n))
         indX = i % num_x
         indY = i / num_x
-        indiMatrix[indX:indX+batch_height, indY:indY+batch_width] = True
+        indiMatrix[indY:indY+batch_height, indX:indX+batch_width] = True
         graph['groups_var'][np.where(indiMatrix.ravel()), i] = True
     return graph
