192e62cafa3429928b0f503cf4b3fc44b241016b
Keith <keithashannon@gmail.com>
2017-12-03 20:34:36 -0600

    Added comments and better variable names

--- FRPCA_GD.py
+++ FRPCA_GD.py
@@ -5,87 +5,79 @@ import timeit
 import scipy
 import numpy.random as rn
 
-def Talpha(Ydat, alpha, d1, d2):
+def Talpha(A, alpha, d1, d2):
     #d1 is the number of rows
-    Y = abs(Ydat)
-    n1 = np.floor(alpha*d1).astype(int)
+    #This takes A to it's incomplete projection where each row/column has 
+    #alpha % non-zero elements
+    #The projection is partial in that it deletes more than it has to
+    Aabs = abs(A)
+    n1 = np.floor(alpha*d1).astype(int) #number of elements allowed in each column
     n2 = np.floor(alpha*d2).astype(int)
-    rows = np.partition(Y, -n2, axis = 1)[:, -n2]
-    cols = np.partition(Y, -n1, axis = 0)[-n1]
-    collim = np.dot(np.ones([d1,1]), cols)
-    rowlim = np.dot(rows, np.ones([1,d2]))
-    limiting = np.maximum(collim, rowlim)
-    TY = np.multiply(np.multiply(Y,(Y >= limiting)),np.sign(Ydat))
-    return TY
+    row_nth_largest = np.partition(Aabs, -n2, axis = 1)[:, -n2] #nth largest element in each row
+    col_nth_largest = np.partition(Aabs, -n1, axis = 0)[-n1]
+    col_rep = np.dot(np.ones([d1,1]), col_nth_largest)
+    row_rep = np.dot(row_nth_largest, np.ones([1,d2]))
+    limiting = np.maximum(col_rep, row_rep)
+    TA = np.multiply(np.multiply(Aabs,(Aabs >= limiting)),np.sign(A))
+    return TA
 
 def UVproj(A, bound, d):
+    #Scales each row down to the bound
     for i in xrange(d):
         rownorm = la.norm(A[i])
         if rownorm > bound:
-#            print(rownorm)
-#            print(bound)
-#            print(A[i])
             A[i] = A[i]*bound/rownorm
-#            print(A[i])
-#            raw_input('')
     return A
             
 
 def FRPCA(Y, alpha, gamma, eta, mu, r, T):
     d1 = np.size(Y[:,0])
     d2 = np.size(Y[0])
-    Sinit = Talpha(Y, alpha, d1, d2)
-    Yrem = Y - Sinit
-    [L, sigma, R] = la.svd(Yrem)
+    S_init = Talpha(Y, alpha, d1, d2)
+    M0 = Y - S_init
+    [L, sigma, R] = la.svd(M0)
     L = L[:,0:r]
     sigmaroot = np.sqrt(sigma[0:r])
-    opnorm = sigmaroot[0]
+    opnorm = sigmaroot[0] #op norm is largest S.V.
     R = R[0:r]
-    U0 = np.multiply(L,sigmaroot)
+    U0 = np.multiply(L,sigmaroot) 
     V0 = np.multiply(sigmaroot,np.transpose(R))
-    Ubound = np.sqrt(2.*mu*r/d1)*opnorm
-#    print(Ubound, 'ubound')
+    Ubound = np.sqrt(2.*mu*r/d1)*opnorm #Used in projection
     Vbound = np.sqrt(2.*mu*r/d2)*opnorm
-#    print(Vbound)
     Ut = U0
     Vt = V0
     gamalph = gamma*alpha
-    Ynorm = la.norm(Y)
+    Ynorm = la.norm(Y)#used for error
     for i in xrange(T):
-#        print(Ut)
-#        print(Vt)
-        Yt = np.dot(Ut, np.transpose(Vt))
-        St = Talpha(Y - Yt, gamalph, d1, d2)
-#        print(St, 'St')
-        lossmat = Yt+St-Y
+        Mt = np.dot(Ut, np.transpose(Vt))    #Current est for low rank matrix
+        St = Talpha(Y - Mt, gamalph, d1, d2) #Current est for sparse matrix
+        lossmat = Mt+St-Y             #difference between est and observation
         error = la.norm(lossmat)/Ynorm
         print(error, 'error at iteration', i)
-#        print(lossmat, 'lossmat')
         UminV = np.dot(np.transpose(Ut), Ut) - np.dot(np.transpose(Vt), Vt)
-#        print(UminV,'umv')
-#        print(np.dot(np.transpose(Ut), Ut))
-#        print(np.dot(np.transpose(Vt), Vt))
         Utnew = Ut - eta*np.dot(lossmat, Vt)-.5*eta*np.dot(Ut, UminV)
         Vtnew = Vt - eta*np.dot(np.transpose(lossmat), Ut)-.5*eta*np.dot(Vt, -UminV)
-#        print(Utnew, 'unew')
-#        print(Vtnew, 'vnew')
         Ut = UVproj(Utnew, Ubound, d1)
         Vt = UVproj(Vtnew, Vbound, d2)
-#        raw_input('a')
     return Ut, Vt
 
 def main():
-    rank = 5
-    d1 = 500
-    d2 = 600
-    alpha = .1
-    a1 = scipy.sparse.random(d1,d2, alpha/2)*rank*5./np.sqrt(d1*d2)
-    a2 = scipy.sparse.random(d1,d2, alpha/2)*rank*-5./np.sqrt(d1*d2)
+    rank = 5 #rank of Y, pixels in video
+    d1 = 500 #rows
+    d2 = 600 #columns
+    alpha = .1 #corruption rate
+    T = 50 #iterations
+    step = .5 #step size
+    gamma = 1.5 #tuning paramenter
+    incoherence = 5. #incoherence of sparse matrix, i.e. smoothness
+    a1 = scipy.sparse.random(d1,d2, alpha/2.)*rank*5./np.sqrt(d1*d2)
+    #a1 and a2 are the corruptions, negative and positive
+    a2 = scipy.sparse.random(d1,d2, alpha/2.)*rank*-5./np.sqrt(d1*d2)
     a = a1+a2
     g = rn.randn(d1,rank)/np.sqrt(d1)
     h = rn.randn(rank,d2)/np.sqrt(d2)
-    Y = np.dot(g,h)+a
-    U, V = FRPCA(Y, alpha, 1.5, .5, 5., rank, 30)
+    Y = np.dot(g,h)+a #test on random low rank corrupted matrix
+    U, V = FRPCA(Y, alpha, gamma, step, incoherence, rank, T)
     return Y, U, V, a
     
 if __name__ == "__main__":
