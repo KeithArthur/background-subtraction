f03d3bde95dc2d371e7ad9296384634474be2a1d
Keith <keithashannon@gmail.com>
2017-12-03 20:10:55 -0600

    FRPCA works good and can be used. No partial observability yet.

--- FRPCA_GD.py
+++ FRPCA_GD.py
@@ -5,8 +5,9 @@ import timeit
 import scipy
 import numpy.random as rn
 
-def Talpha(Y, alpha, d1, d2):
+def Talpha(Ydat, alpha, d1, d2):
     #d1 is the number of rows
+    Y = abs(Ydat)
     n1 = np.floor(alpha*d1).astype(int)
     n2 = np.floor(alpha*d2).astype(int)
     rows = np.partition(Y, -n2, axis = 1)[:, -n2]
@@ -14,23 +15,27 @@ def Talpha(Y, alpha, d1, d2):
     collim = np.dot(np.ones([d1,1]), cols)
     rowlim = np.dot(rows, np.ones([1,d2]))
     limiting = np.maximum(collim, rowlim)
-    TY = np.multiply(Y,(Y >= limiting))
+    TY = np.multiply(np.multiply(Y,(Y >= limiting)),np.sign(Ydat))
     return TY
 
 def UVproj(A, bound, d):
     for i in xrange(d):
         rownorm = la.norm(A[i])
         if rownorm > bound:
+#            print(rownorm)
+#            print(bound)
+#            print(A[i])
             A[i] = A[i]*bound/rownorm
+#            print(A[i])
+#            raw_input('')
     return A
             
 
-def FRPCA(Y, alpha, gamma, eta, mu, T):
+def FRPCA(Y, alpha, gamma, eta, mu, r, T):
     d1 = np.size(Y[:,0])
     d2 = np.size(Y[0])
     Sinit = Talpha(Y, alpha, d1, d2)
     Yrem = Y - Sinit
-    r = la.matrix_rank(Y)
     [L, sigma, R] = la.svd(Yrem)
     L = L[:,0:r]
     sigmaroot = np.sqrt(sigma[0:r])
@@ -38,27 +43,49 @@ def FRPCA(Y, alpha, gamma, eta, mu, T):
     R = R[0:r]
     U0 = np.multiply(L,sigmaroot)
     V0 = np.multiply(sigmaroot,np.transpose(R))
-    Ubound = np.sqrt(2*mu*r/d1)*opnorm
-    Vbound = np.sqrt(2*mu*r/d2)*opnorm
+    Ubound = np.sqrt(2.*mu*r/d1)*opnorm
+#    print(Ubound, 'ubound')
+    Vbound = np.sqrt(2.*mu*r/d2)*opnorm
+#    print(Vbound)
     Ut = U0
     Vt = V0
     gamalph = gamma*alpha
+    Ynorm = la.norm(Y)
     for i in xrange(T):
-        St = Talpha(Y - np.dot(Ut, np.transpose(Vt)), gamalph, d1, d2)
-        lossmat = np.dot(Ut, np.transpose(Vt))+St-Y
+#        print(Ut)
+#        print(Vt)
+        Yt = np.dot(Ut, np.transpose(Vt))
+        St = Talpha(Y - Yt, gamalph, d1, d2)
+#        print(St, 'St')
+        lossmat = Yt+St-Y
+        error = la.norm(lossmat)/Ynorm
+        print(error, 'error at iteration', i)
+#        print(lossmat, 'lossmat')
         UminV = np.dot(np.transpose(Ut), Ut) - np.dot(np.transpose(Vt), Vt)
+#        print(UminV,'umv')
+#        print(np.dot(np.transpose(Ut), Ut))
+#        print(np.dot(np.transpose(Vt), Vt))
         Utnew = Ut - eta*np.dot(lossmat, Vt)-.5*eta*np.dot(Ut, UminV)
         Vtnew = Vt - eta*np.dot(np.transpose(lossmat), Ut)-.5*eta*np.dot(Vt, -UminV)
+#        print(Utnew, 'unew')
+#        print(Vtnew, 'vnew')
         Ut = UVproj(Utnew, Ubound, d1)
         Vt = UVproj(Vtnew, Vbound, d2)
+#        raw_input('a')
     return Ut, Vt
 
 def main():
-    a = scipy.sparse.random(100,300, .02)
-    g = rn.rand(100,1)
-    h = rn.rand(1,300)
+    rank = 5
+    d1 = 500
+    d2 = 600
+    alpha = .1
+    a1 = scipy.sparse.random(d1,d2, alpha/2)*rank*5./np.sqrt(d1*d2)
+    a2 = scipy.sparse.random(d1,d2, alpha/2)*rank*-5./np.sqrt(d1*d2)
+    a = a1+a2
+    g = rn.randn(d1,rank)/np.sqrt(d1)
+    h = rn.randn(rank,d2)/np.sqrt(d2)
     Y = np.dot(g,h)+a
-    U, V = FRPCA(Y, .04, 3, .01, 10, 100)
+    U, V = FRPCA(Y, alpha, 1.5, .5, 5., rank, 30)
     return Y, U, V, a
     
 if __name__ == "__main__":
