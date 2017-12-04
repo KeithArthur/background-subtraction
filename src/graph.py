1c1cd5aea1add8f14ff2db178ac0d91f0ddb2649
Dany Haddad <danyhaddad43@gmail.com>
2017-12-04 12:05:49 -0600

    use sparse matrix

--- src/graph.py
+++ src/graph.py
@@ -13,9 +13,9 @@ def build_graph(frame_dimensions, batch_dimensions):
     num_y = frame_width_n - batch_width + 1
     num_groups = num_x * num_y
     graph = {'eta_g': np.ones(num_groups, dtype=project_float),
-             'groups': sp.csc_matrix(np.zeros((num_groups, num_groups)),
+             'groups': sp.csc_matrix((num_groups, num_groups),
                                      dtype=np.bool),
-             'groups_var': sp.csc_matrix(np.zeros((frame_height_m * frame_width_n, num_groups), dtype=np.bool), dtype=np.bool)}
+             'groups_var': sp.csc_matrix((frame_height_m * frame_width_n, num_groups), dtype=np.bool)}
     for i in range(0, num_groups):
         indX = i / num_y
         indY = i % num_y
